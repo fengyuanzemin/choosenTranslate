b7baf0f30cab8328ed141a1a401b5700c42ba228
fengyuanzemin <fengyuanzemin@gmail.com>
2016-07-13 18:13:54 +0800

    添加点击事件

---
+++ popup.js
@@ -0,0 +1,10 @@
+"use strict";
+document.querySelector('#filter').addEventListener('click',function(){
+	
+});
+document.querySelector('#pagination').addEventListener('click',function(){
+	
+});
+document.querySelector('#shanbay').addEventListener('click',function(){
+	
+});
