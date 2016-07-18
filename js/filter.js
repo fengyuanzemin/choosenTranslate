efdf57076cd8df115adcabc34c51817b5c6365a5
fengyuanzemin <fengyuanzemin@gmail.com>
2016-07-18 20:17:25 +0800

    在当前页面完成过滤，只不过有个bug解决不了，就是过一段时间后会有js加载进来，结果就是新添几个元素

--- js/filter.js
+++ js/filter.js
@@ -1,48 +1,41 @@
 // 移除一些不必要的元素
-$('.js-navigation-header').remove();
 $('body > header').css('display', 'none');
-$('footer').css('display', 'none');
-$('aside').css('display', 'none');
-$('section').css('display', 'none');
-$("iframe").css('display', 'none');
-$('popover').css('display', 'none');
-// $('noscript,script').remove();
-
 // 以body元素开始
 var element = document.querySelector('body');
 var list = [];
 var temp = [];
-var filter = /ad-|footer|sub|message/;
+var filter = /ad-|footer|sub|message|meta|twitter|labels/;
 var filter_head = /head/;
 
 getClassName(element);
 
 function getClassName(ele) {
-    var i, j;
-
+    var i, j, count = 0;
+    // 有子元素
     if (ele.children.length > 0) {
-        // console.log(ele.classList);
-        for (j = 0; j < ele.classList.length; j++) {
-            if (filter.test(ele.classList[j]) && !filter_head.test(ele.classList[j])) {
-                list.push(ele.classList[j]);
+        for (i = 0; i < ele.classList.length; i++) {
+            if (filter.test(ele.classList[i]) && !filter_head.test(ele.classList[i])) {
+                list.push(ele.classList[i]);
+                count++;
+                break;
             }
         }
-        //     if (ele.className && filter.test(ele.className) && !filter_head.test(ele.className)) {
-        //         list.push(ele.classList[0]);
-        //     } else {
-        //         for (i = 0; i < ele.children.length; i++) {
-        //             getClassName(ele.children[i]);
-        //         }
-        //     }
-        // } else if (ele.children.length === 0) {
-        //     if (ele.className && filter.test(ele.className) && !filter_head.test(ele.className)) {
-        //         list.push(ele.classList[0]);
-        //     }
+        // 未找到符合条件的class，进入子元素找
+        if (count === 0) {
+            for (i = 0; i < ele.children.length; i++) {
+                getClassName(ele.children[i]);
+            }
+        }
+    } else if (ele.children.length === 0) {
+        for (i = 0; i < ele.classList.length; i++) {
+            if (filter.test(ele.classList[i]) && !filter_head.test(ele.classList[i])) {
+                list.push(ele.classList[i]);
+                break;
+            }
+        }
     }
 }
 // list里面有所有的class名
-console.log(list);
 for (var j in list) {
-    console.log(list[j]);
     $('.' + list[j]).css('display', 'none');
 }
