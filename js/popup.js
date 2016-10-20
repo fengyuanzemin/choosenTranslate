628787659f2caf66eabe9e96aa1d76e5205418e1
fengyuanzemin <fengyuanzemin@gmail.com>
2016-10-20 20:02:36 +0800

    change ES6

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,10 @@
 // 扇贝查词
-var shanbayOpen = localStorage.getItem('shanbay') ? localStorage.getItem('shanbay') : true;
-var shanbay = $('.shanbay');
-if (shanbayOpen == 'false') {
+const shanbayOpen = localStorage.getItem('shanbay') ? localStorage.getItem('shanbay') : true;
+const shanbay = $('.shanbay');
+if (shanbayOpen === 'false') {
     shanbay.addClass('closed');
     shanbay.text('开启屏幕查词');
-} else if (shanbayOpen == 'true') {
+} else if (shanbayOpen === 'true') {
     shanbay.removeClass('closed');
     shanbay.text('关闭屏幕查词');
 }
--- js/shanbay.js
+++ js/shanbay.js
@@ -5,7 +5,7 @@
 
 // 取得shanbay是否打开
 chrome.runtime.sendMessage({method: "getShanbay"}, function (response) {
-    var isShanbay = response.data;
+    const isShanbay = response.data;
     if (isShanbay == 'true') {
         shanbay();
     }
@@ -18,11 +18,11 @@ function shanbay() {
 
     // 查询单词
     function searchingSelectedText() {
-        var text = window.getSelection().toString().trim().match(/^[a-zA-Z\s']+$/);
+        const text = window.getSelection().toString().trim().match(/^[a-zA-Z\s']+$/);
         if (undefined != text && null != text && 0 < text.length) {
-            var API = 'https://api.shanbay.com/bdc/search/?word=';
+            const API = 'https://api.shanbay.com/bdc/search/?word=';
             // var url = API + normalize(text);
-            var url = API + text;
+            const url = API + text;
 
             $.ajax({
                 url: url,
@@ -42,48 +42,50 @@ function shanbay() {
     }
 
     function popover(alldata) {
-        var data = alldata.data;
-        var html = '';
+        const data = alldata.data;
+        let html = '';
 
         getSelectionOffset(function (left, top) {
-            var html_begin = '<div class="shanbay_popover">';
-            var html_end = '</div>'
+            const html_begin = '<div class="shanbay_popover">';
+            const html_end = '</div>';
+            let html_content;
             if (alldata.status_code === 0) {
                 // 查找成功
-                var html_content = '<div class="shanbay_container">' +
-                    '<div class="shanbay_title">' +
-                    '<span class="shanbay_name">' + data.audio_name + '</span><br/>' +
-                    '<span class="shanbay_uk speak">UK ' +
-                    '<span class="uk_pron">[' + data.pronunciations.uk + ']</span>' +
-                    ' <span class="icon-speak"></span>' +
-                    '</span>' +
-                    '<span class="shanbay_us speak">US ' +
-                    '<span class="us_pron">[' + data.pronunciations.us + ']</span>' +
-                    ' <span class="icon-speak"></span>' +
-                    '</span>' +
-                    '</div>' +
-                    '<div class="shanbay_definitions">' + data.definition.split('\n').join('<br/>') +
-                    '</div></div>';
+                html_content =
+                    `<div class="shanbay_container">
+                        <div class="shanbay_title">
+                            <span class="shanbay_name">${data.audio_name}</span><br/>
+                            <span class="shanbay_uk speak">UK 
+                                <span class="uk_pron">[ ${data.pronunciations.uk} ]</span>
+                                <span class="icon-speak"></span>
+                            </span>
+                            <span class="shanbay_us speak">US 
+                                <span class="us_pron">[ ${data.pronunciations.us} ]</span>
+                                <span class="icon-speak"></span>
+                            </span>
+                        </div>
+                    <div class="shanbay_definitions">${data.definition.split('\n').join('<br/>')}</div>
+                    </div>`;
 
             } else {
                 // 未找到该单词
-                var html_content = '<div class="shanbay_container">' +
-                    '<div class="shanbay_title">' + alldata.msg +
-                    '</div>' +
-                    '</div>';
+                html_content =
+                    `<div class="shanbay_container">
+                        <div class="shanbay_title">${alldata.msg}</div>
+                    </div>`;
 
             }
 
-            var h = $(window).scrollTop() + $(window).height();
-            var body = $('body');
+            const h = $(window).scrollTop() + $(window).height();
+            const body = $('body');
             if (h - 200 < top && h >= top) {
                 // 在最底部
-                html = html_begin + html_content + '<div class="triangle-down"></div>' + html_end;
+                html = `${html_begin}${html_content}<div class="triangle-down"></div>${html_end}`;
                 $('.shanbay_popover').remove();
                 body.append(html);
                 setPopoverPosition(left, top - $('.shanbay_popover').height() - 5);
             } else { // 默认在上部
-                html = html_begin + '<div class="triangle-up"></div>' + html_content + html_end;
+                html = `${html_begin}<div class="triangle-up"></div>${html_content}${html_end}`;
                 $('.shanbay_popover').remove();
                 body.append(html);
                 setPopoverPosition(left, top);
@@ -92,18 +94,19 @@ function shanbay() {
             // 在最左部，暂时认为左右不用管
 
         });
-        var body = $('body');
-        body.on('click', function () {
+        const body = $('body');
+        body.on('click', (e) => {
+            e.stopPropagation();
             hidePopover();
         });
-        body.on('click', '.shanbay_popover', function (e) {
+        body.on('click', '.shanbay_popover', (e) => {
             e.stopPropagation();
         });
-        body.on('click', '.shanbay_us', function (e) {
+        body.on('click', '.shanbay_us', (e) => {
             e.preventDefault();
             playAudio(data.us_audio);
         });
-        body.on('click', '.shanbay_uk', function (e) {
+        body.on('click', '.shanbay_uk', (e) => {
             e.preventDefault();
             playAudio(data.uk_audio);
         });
@@ -115,17 +118,17 @@ function shanbay() {
 
     function getSelectionOffset(callback) {
         // 取默认值
-        var left = window.innerWidth / 2;
-        var top = window.innerHeight / 2;
+        let left = window.innerWidth / 2;
+        let top = window.innerHeight / 2;
 
         var selection = window.getSelection();
         if (0 < selection.rangeCount) {
-            var range = selection.getRangeAt(0);
-            var dummy = document.createElement('span');
+            const range = selection.getRangeAt(0);
+            const dummy = document.createElement('span');
             dummy.style.display = 'inline-block';
             range.insertNode(dummy);
             // 获得选区的位置
-            var clientRect = dummy.getBoundingClientRect();
+            const clientRect = dummy.getBoundingClientRect();
             left = clientRect.left + document.body.scrollLeft - 50;
             top = clientRect.top + clientRect.height + document.body.scrollTop;
 
@@ -156,6 +159,6 @@ function shanbay() {
     }
 
     function playAudio(audio_url) {
-        chrome.runtime.sendMessage({method: "playAudio", data: {audio_url: audio_url}})
+        chrome.runtime.sendMessage({method: "playAudio", data: {audio_url: audio_url}});
     }
 }
